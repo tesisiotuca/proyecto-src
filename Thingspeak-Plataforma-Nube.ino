5a980f71bb09ca548eb53d41ae30845c75f0f6d9
luisddw <luera19@gmail.com>
2020-06-13 21:56:30 -0600

    Thingspeak-Plataforma-Nube.ino

--- Thingspeak-Plataforma-Nube.ino
+++ Thingspeak-Plataforma-Nube.ino
@@ -2,25 +2,25 @@
 
 //Pines de los Sensores
 const int PIN_SENSOR_TEMPERATURA = A2;
-const int PIN_SENSOR_HUMEDAD = A4;
-const int PIN_SENSOR_LED = A0;
+const int PIN_SENSOR_NIVEL_DAGUA = A4;
+const int PIN_SENSOR_MOTOR_OPERANDO = A0;
 
 //Utiliza LEDs para indicar la condición de funcionamiento del ESP8266
-const int PIN_LED_ERROR = 13;
-const int PIN_LED_OK = 12;
+const int PIN_LED_ERROR = 13;		
+const int PIN_LED_OK   = 12;		
 
 //Pines que están conectados a la pantalla LCD
 const int PIN_LCD_RS = 2;
-const int PIN_LCD_ENABLE =3;
+const int PIN_LCD_ENABLE = 3;
 const int PIN_LCD_DB4 = 4;
 const int PIN_LCD_DB5 = 5;
 const int PIN_LCD_DB6 = 6;
 const int PIN_LCD_DB7 = 7;
 
 // Parámetros del puerto serial
-const unsigned long SERIAL_BAUDRATE = 115200L; //Velocidad de Transmisión
-const unsigned long SERIAL_TIMEOUT  = 5000L;
-const unsigned long TIEMPO_MOSTRANDO_RESPUESTA = 4000L; 
+const unsigned long SERIAL_BAUDRATE = 115200L;//Velocidad de Transmisión
+const unsigned long SERIAL_TIMEOUT  = 5L;//5000L
+const unsigned long TIEMPO_MOSTRANDO_RESPUESTA = 4L;//4000L
 bool _mostrarComandosDisplay;
 
 // Características de LCD
@@ -33,143 +33,152 @@ LiquidCrystal _lcd(PIN_LCD_RS, PIN_LCD_ENABLE,
                    PIN_LCD_DB4, PIN_LCD_DB5, PIN_LCD_DB6, PIN_LCD_DB7);
 
 // Redes de WiFi del simulador
-String _ssidName     = "";    // Nombre de la red
-String _ssidPassword = "";            // Sin contraseña
-int _tcpHttpPort     = 80;            // Puerto HTTP en la conexión TCP
+String _ssidName     = "Simulator Wifi";		// Nombre de la red
+String _ssidPassword = ""; 						// Sin contraseña
+int _tcpHttpPort     = 80; 						// Puerto HTTP en la conexión TCP
 
-
-// Datos para accceder al sitio web
-String _siteHost    = "api.thingspeak.com";    // URL
-String _siteAPPID   = "4BUKOQC1DMO51H0V";   // Appkey en el sitio web de Thingspeak
+// Datos para accceder al sitio web Thinspeak
+// https://api.thingspeak.com/update?api_key=4BUKOQC1DMO51H0V&field1=0
+String _siteHost    = "api.thingspeak.com";		// URL
+String _siteAPPID   = "9US4IDX84FTJT07I";		// Appkey en el sitio web de Thingspeak
 String _siteURIbase = "/update?api_key=" + _siteAPPID;
 String _siteField1  = "&field1=";
 String _siteField2  = "&field2=";
 String _siteField3  = "&field3=";
 
-
 // Parâmetros para a lectura da temperatura
-const int TEMPERATURA_CELSIUS_MINIMA = -40;    // Menor temperatura de sensor [°C]
-const int TEMPERATURA_CELSIUS_MAXIMA = 125;   // Mayor temperatura de sensor [°C]
+const int TEMPERATURA_CELSIUS_MINIMA = -40;		// Menor temperatura de sensor [°C]
+const int TEMPERATURA_CELSIUS_MAXIMA = 125;		// Mayor temperatura de sensor [°C]
 
-int _lecturaMinima;      // Guardando menor lectura (-40°C)
-int _lecturaMaxima;     // Guardando maior lectura (+125°C)
+int _lecturaMinima;			
+int _lecturaMaxima;			
+
+
+// Inicializa la lógica del LCD
+// Abre el canal de comunicación TCP via WiFi
 
 void setup() {
   // Inicializa la lógica del LED del error
   pinMode(PIN_LED_ERROR, OUTPUT);
   digitalWrite(PIN_LED_ERROR, LOW);
   
-  // Inicializando la lógica de los sensores
+  // Inicializando a lógica dos sensores
   pinMode(PIN_SENSOR_TEMPERATURA, INPUT);
-  pinMode(PIN_SENSOR_HUMEDAD, INPUT);
-  pinMode(PIN_SENSOR_LED, INPUT);
+  pinMode(PIN_SENSOR_NIVEL_DAGUA, INPUT);
+  pinMode(PIN_SENSOR_MOTOR_OPERANDO, INPUT);
   
   // Inicializa el LCD
   _lcd.begin(LCD_NUMERO_COLUMNAS,LCD_NUMERO_LINEAS);
   _lcd.print("TESIS IoT");
   _lcd.setCursor(0,1);
   _lcd.print("App-Sensores");
-  delay(2000);  // Tiempo de mensaje visible en pantalla
+  delay(2000);	// Deixa a mensagem inicial visível
   
   // Prepara para calibrar la temperatura
   // Inicializa con los valores del ejercicio con el sensor de temperatura.
+  // https://www.tinkercad.com/things/kGOeHnwDf2S
   
-  _lecturaMinima = 20;  // Use 9999 si desea calibrar automáticamente
+  _lecturaMinima = 20;	// Use 9999 si desea calibrar automáticamente
   _lecturaMaxima = 358; // Use -9999 si desea realizar la calibración automática
 
   //Inicializa la lógica de ESP8266
   pinMode(PIN_LED_OK, OUTPUT);
   Serial.begin(SERIAL_BAUDRATE);
   Serial.setTimeout(SERIAL_TIMEOUT);
+  _mostrarComandosDisplay = false;		
   
-  
-  // Confirma que el ESP8266 está operativo al inicializar el módulo
+   // Confirma que el ESP8266 está operativo al inicializar el módulo
   bool esp8266OK = sendCommandTo8266("AT+RST", "ready");
-  if (!esp8266OK) {           
-    digitalWrite(PIN_LED_OK, LOW);   
+  if (!esp8266OK) {						
+  	digitalWrite(PIN_LED_OK, LOW);		
     _lcd.setCursor(0,1);
-    _lcd.print("ERROR en ESP8266!");
-    while (1) { // Bloquea la ejecución del programa.
-      delay(500);
+  	_lcd.print("ERROR en ESP8266!");
+    // Bloquea la ejecución del programa.
+    while (1) {	
+      delay(5);//delay(500)
       _lcd.noDisplay();
-      delay(500);
+      delay(5);//delay(500)
       _lcd.display();
     }
-  } else {                // SÍ, se enciende el LED verde
-    digitalWrite(PIN_LED_OK, HIGH);
+  } else {								// SÍ, se enciende el LED verde
+  	digitalWrite(PIN_LED_OK, HIGH);
+    
+    
     // Conéctese al WiFi Simulator usando el comando AT + CWJAP
     String loginWiFi = "AT+CWJAP=\"" + _ssidName + "\",\"" + _ssidPassword + "\"";
+    //AT+CWJAP="",""
     sendCommandTo8266(loginWiFi, "OK");
 
     // Abre el canal de comunicación TCP con el sitio web utilizando el comando AT + CIPSTART
-    String acessoTCP = "AT+CIPSTART=\"TCP\",\"" + _siteHost + "\"," + String(_tcpHttpPort);
-    sendCommandTo8266(acessoTCP, "OK");
-
-    delay(2000);
-   } 
-
+    String accessoTCP = "AT+CIPSTART=\"TCP\",\"" + _siteHost + "\"," + String(_tcpHttpPort);
+    sendCommandTo8266(accessoTCP, "OK");
+	  //AT+CIPSTART="TCP","api.thingspeak,com","80"
+    //Para servidor local cambiar puerto 80 a puerto 3000 y que lleve los ":"
+    
+    delay(2);//delay(2000)
+    _mostrarComandosDisplay = false;	
+  } 
 }
 
-//Envía los datos de los sensores periodicamente a la web
+
+//Envía los datos de los sensores periodicamente a la web Thingspeak
 void loop() {
-  // Lee el sensor de temperatura (precisión 4,88 mV)
+    // Lee el sensor de temperatura (precisión 4,88 mV)
   int lecturaSensor = analogRead(PIN_SENSOR_TEMPERATURA);
   // Realiza la autocalibración
-  if (lecturaSensor > _lecturaMaxima)      
-      _lecturaMaxima = lecturaSensor;
-  if (lecturaSensor < _lecturaMinima)
-      _lecturaMinima = lecturaSensor;
-  
+  if (lecturaSensor > _lecturaMaxima)			_lecturaMaxima = lecturaSensor;
+  if (lecturaSensor < _lecturaMinima)			_lecturaMinima = lecturaSensor;
   // Convierte la lectura del sensor a temperatura Celsius
-  int temperaturaCelsius = map(lecturaSensor,_lecturaMinima,_lecturaMaxima,TEMPERATURA_CELSIUS_MINIMA,TEMPERATURA_CELSIUS_MAXIMA);
+  int temperaturaCelsius = map(lecturaSensor, 
+                               _lecturaMinima, _lecturaMaxima,
+                               TEMPERATURA_CELSIUS_MINIMA, TEMPERATURA_CELSIUS_MAXIMA);
   String temperaturaCelsiusTexto = numberToString(temperaturaCelsius);
-
  
   // Sensor de nivel de Humedad
-  int nivelHumedad = analogRead(PIN_SENSOR_HUMEDAD);
-  int nivelHumedadPorc = map(nivelHumedad,0, 1023, 0, 100);
-  String nivelHumedadTexto = numberToString(nivelHumedadPorc);
+  int nivelHumedad = analogRead(PIN_SENSOR_NIVEL_DAGUA);
+  int nivelHumedadPorcentaje = map(nivelHumedad, 
+                                    0, 1023, 0, 100);
+  String nivelHumedadTexto = numberToString(nivelHumedadPorcentaje);
   
-  
-  
-  // Sensor do motor
-  bool led = digitalRead(PIN_SENSOR_LED);
-  String ledTexto = led ? "1" : "0";
-  
-  
+  // Sensor de motor
+  bool motorLed = digitalRead(PIN_SENSOR_MOTOR_OPERANDO);
+  String motorLedTexto = motorLed ? "1" : "0";
   
   // Muestra el estado de los sensores, si no usa la pantalla LCD para los comandos AY
   if (!_mostrarComandosDisplay)
-    mostrarDisplayLCD(temperaturaCelsiusTexto, nivelHumedadTexto, ledTexto);
-
+  	mostraDisplayLCD(temperaturaCelsiusTexto, nivelHumedadTexto, motorLed);
   
-  // Enviar sensores a la web
-  enviarSensores(temperaturaCelsiusTexto, nivelHumedadTexto, ledTexto);
-  
+  // Enviar sensores a la nube
+  enviarSensores(temperaturaCelsiusTexto, nivelHumedadTexto, motorLedTexto);
+  _mostrarComandosDisplay = false;
   
   // Enviar sensores a cada 1 segundo
-  delay(1000);
-
+  delay(10);//delay(1000)
 }
 
-
-
 //FUNCION ENVIAR SENSORES
-bool enviarSensores(String temperatura, String nivel, String led) {
+bool enviarSensores(String temperatura, String nivel, String motor) {
   bool sucesso = true;
   
    // Construir la solicitud HTTP
-  String urlCompleta = _siteURIbase + 
-               _siteField1 + temperatura +
+  String uriCompleta = _siteURIbase + 
+    				   _siteField1 + temperatura +
                        _siteField2 + nivel +
-                       _siteField3 + led;
-    
-  String httpPacket = "GET " + urlCompleta + " HTTP/1.1\r\nHost: " + _siteHost + "\r\n\r\n";
+                       _siteField3 + motor;
+  
+  //uriCompleta=/update?api_key=4BUKOQC1DMO51H0V&field1=valor&field2=valor&field3=valor
+  
+  String httpPacket = "GET " + uriCompleta + " HTTP/1.1\r\nHost: " + _siteHost + "\r\n\r\n";
+  //httpPacket = GET /update?api_key=4BUKOQC1DMO51H0V&field1=valor&field2=valor&field3=valor HTTP/1.1
+  //Host:api.thingspeak.com 
+  
   int length = httpPacket.length();
+  //length = 104
   
   // Enviando el tamaño del paquete a ESP8266
-  String tamanoPaquete = "AT+CIPSEND=" + numberToString(length);  
-  sucesso &= sendCommandTo8266(tamanoPaquete, ">");
+  String longitudPaquete = "AT+CIPSEND=" + numberToString(length);  
+  //longitudPaquete=AT+CIPSEND=104
+  sucesso &= sendCommandTo8266(longitudPaquete, ">");
   sucesso &= sendCommandTo8266(httpPacket, "SEND OK");
   
   return sucesso;
@@ -177,102 +186,97 @@ bool enviarSensores(String temperatura, String nivel, String led) {
 
 
 //FUNCION DE MOSTRAR EN DISPLAY LCD
-void mostrarDisplayLCD(String temperatura, String nivel, bool led) {
-  //Imprimiendo en pantalla el valor de la temperatura
+void mostraDisplayLCD(String temperatura, String nivel, bool motor) {
+  //Imprimiendo en primera linea valor de la temperatura y On
   _lcd.setCursor(0,0);
-  _lcd.print("Temp:"); 
+  _lcd.print("Temp: "); 
   _lcd.print(temperatura);  
   _lcd.print('\xB2'); 
-  _lcd.print("C ");
+  _lcd.print("C  ");
+  if (motor)			_lcd.print("ON      ");
+  else					_lcd.print("        ");
   
-  //Imprimiendo en pantalla el valor de Humedad
+  //Imprimiendo en pantalla el valor de Humedad y Off
   _lcd.setCursor(0,1);
-  _lcd.print("Hum:"); 
+  _lcd.print("Hum: "); 
   _lcd.print(nivel);  
-  _lcd.print("%  ");
-  
-  //Imprimiendo en pantalla el valor de motor
-  if (led)
-  _lcd.print("ON ");
-  else
-  _lcd.print("   ");
-  
-  if (!led)
-  _lcd.print("OFF ");
-  else
-  _lcd.print("   ");
+  _lcd.print("%   ");
+  if (!motor)			_lcd.print("OFF     ");
+  else					_lcd.print("        ");
 }
 
 
 //FUNCION QUE ENVIA EL COMANDO AT A LA ESP8266 Y ESPERA UNA CADENA DE RESPUESTA.
 //SI SE PRODUCE UN ERROR, SE ENCIENDE EL LED ROJO.
 
-bool sendCommandTo8266(String comando, char * aguardar) {
+bool sendCommandTo8266(String comando, char * esperar) {
   bool sucesso = false;
   
   // Envía el comando a ESP8266 y espere a que se procese.
   Serial.println(comando);
   Serial.flush();
-  delay(50);
+  delay(5); //delay(50)
   
-  // Muestra el comando AT en el display LCD
-  if (_mostrarComandosDisplay)  { // Está mostrando AT en display LCD?
-    imprimeDisplay(1, comando);   // SIM, mostra el comando enviado
-    imprimeDisplay(2, "");        // Aguarda la respuesta
+  // Mostra o comando AT no display LCD, se habilitado para tal
+  if (_mostrarComandosDisplay)	{	
+    imprimeDisplay(1, comando);		
+  	imprimeDisplay(2, "");				
   }
   
   // Verifica si hay una respuesta a ser guardada
-  if (0 == aguardar[0])
+  if (0 == esperar[0])
     sucesso = true;
-  else if (_mostrarComandosDisplay) {  // Mostrando AT en la pantalla LCD?
-    sucesso = verificarRespuesta(aguardar);
-    delay(TIEMPO_MOSTRANDO_RESPUESTA);  // SIM, permite a lectura
+  else if (_mostrarComandosDisplay) {	
+    sucesso = findMostrando(esperar);
+    delay(TIEMPO_MOSTRANDO_RESPUESTA);	
   } else
-    sucesso = Serial.find(aguardar);  // NO, solo espera la respuesta
+    sucesso = Serial.find(esperar);	
     
   digitalWrite(PIN_LED_ERROR, !sucesso); //Error, en ESP8266 se enciende LED Rojo
   return sucesso;
 }
 
 
-bool verificarRespuesta(char * aguardar) {
+
+bool findMostrando(char * esperar) {
   bool sucesso = false;
   
-  if (0 == aguardar[0])     
-    sucesso = true;       
+  if (0 == esperar[0]) 		
+  	sucesso = true;				
   else {
     unsigned long tiempoLimite = millis() + SERIAL_TIMEOUT;
-    int index = 0;
+  	int index = 0;
     String respuesta = "";
     while (millis() <= tiempoLimite) {
-      if (!Serial.available())    // recibe algo?
-        delay(1);         // NO, espere 1ms
-      else {            
+      if (!Serial.available())		
+      	delay(1);					
+      else {						
         // SIM, procesa una respuesta
         char recibido = Serial.read();
-        // Deja los caracteres de control visibles en la pantalla LCD
-        if ('\r' == recibido)      respuesta += '\xFE';
-        else if ('\n' == recibido)   respuesta += '\xFF';
-        else if (recibido < ' ')     respuesta += '\xFC';
-        else              
-        respuesta += recibido;
         
-        if (recibido != aguardar[index]) { // encontro el caracter en la cadena?
-          index = 0;            // no, continua esperando
-        } else {              // SIM, verifica si termino
+        // Deja los caracteres de control visibles en la pantalla LCD
+        if ('\r' == recibido)			respuesta += '\xFE';
+        else if ('\n' == recibido)		respuesta += '\xFF';
+        else if (recibido < ' ')			respuesta += '\xFC';
+        else 							respuesta += recibido;
+        // Verifica si encontro el caracter en la cadena
+        if (recibido != esperar[index]) {	
+          index = 0;						
+        } else {							
           index++;
-          if (0 == aguardar[index]) {     // Encontré lo que buscaba?
-            sucesso = true;           
+          if (0 == esperar[index]) {			
+            sucesso = true;						
             break;
           }
         }
       }
-    } 
-    imprimeDisplay(0, respuesta);  // Muestra la respuesta
-  } 
+    }	
+    imprimeDisplay(0, respuesta);	
+  }	
   
   return sucesso;
 }
+
 
 /**
  * @brief Escribe mensajes intercambiados con el ESP8266 en la pantalla LCD
@@ -285,14 +289,14 @@ bool verificarRespuesta(char * aguardar) {
  * texto Texto a escribir en la pantalla LCD.
  */
 
-
 void imprimeDisplay(byte comoEscribir, String texto) {
-  if (1 == comoEscribir)      _lcd.clear();
-  else if (2 == comoEscribir)   limpiarlineaLCD(1);
-  else if (3 == comoEscribir)   limpiarlineaLCD(0);
+  if (1 == comoEscribir)		 	_lcd.clear();
+  else if (2 == comoEscribir)		limpiarlineaLCD(1);
+  else if (3 == comoEscribir)		limpiarlineaLCD(0);
   String textoC = texto.substring(0, LCD_NUMERO_COLUMNAS);
   _lcd.print(textoC);
 }
+
 
 //borra la línea de la pantalla LCD, dejando el cursor encendido
 void limpiarlineaLCD(byte linea) {
